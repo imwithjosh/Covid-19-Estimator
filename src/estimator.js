73b886deffae4afcce86b48649f4375f1dc8951b
oyewale tayo <tayooyewale@gmail.com>
2020-04-11 23:16:09 +0100

    pushing code further

--- src/estimator.js
+++ src/estimator.js
@@ -1,11 +1,32 @@
 const covid19ImpactEstimator = (data) =>
+
+const dat = {
+    region: {
+    name: "Africa",
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+    }
+
 {
     const input = data;
     return {
     data:input,
     impact:{},
-    severeImpact:{}
+    severeImpact:{
 
+        currentlyInfected: reportedCases * 50;
+    }
+    currentlyInfected:{
+
+        impact.currentlyInfected = reportedCases * 10;
+    }
     };
     
 };
