99453523bbd603eb38bf54448bbb537e6b13f091
oyewale tayo <tayooyewale@gmail.com>
2020-04-12 10:02:43 +0100

    Challnege 1 completed

--- src/estimator.js
+++ src/estimator.js
@@ -1,39 +1,30 @@
-const covid19ImpactEstimator = (data) =>
-
-const dat = {
-    region: {
-    name: "Africa",
-    avgAge: 19.7,
+const input = {
+    region:{
+    name: 'Africa',
+    avAge:19.7,
     avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
+    avgDailyIncomePopulation:0.71
     },
-    periodType: "days",
-    timeToElapse: 58,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-    }
-
-{
-    const input = data;
-    return {
-    data:input,
-    impact:{},
-    severeImpact:{
-
-        currentlyInfected: reportedCases * 50;
-    }
-    currentlyInfected:{
-
-        impact.currentlyInfected = reportedCases * 10;
-    }
+    periodType: 'days',
+    timeToELapse:58,
+    reportedCases:674,
+    population:66622705,
+    totalHospitalBeds:1380614
     };
     
-};
-
-
-
-
-
-
-export default covid19ImpactEstimator;
+    const covid19ImpactEstimator = (data) => {
+    const input = data;
+    const impact = {}
+    const severeImpact = {} 
+      // Challenge 1 
+      impact.currentlyInfected = data.reportedCases * 10;
+      severeImpact.currentlyInfected = data.reportedCases * 50;
+      impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** Math.floor(data.timeToElaspe / 3))
+      severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** Math.floor(data.timeToElaspe /3))
+      return {data: input,
+             impact: impact,
+             severeImpact: severeImpact
+             }
+    }
+    
+    export default covid19ImpactEstimator;
