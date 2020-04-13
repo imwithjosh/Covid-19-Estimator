97c65d95e1b840d81e77863a646e10986377a02e
oyewale tayo <tayooyewale@gmail.com>
2020-04-13 08:50:49 +0100

    fixing issues with time to elapse

--- src/estimator.js
+++ src/estimator.js
@@ -1,16 +1,16 @@
 const input = {
-    region:{
+  region: {
     name: 'Africa',
     avAge: 19.7,
     avgDailyIncomeInUSD: 5,
     avgDailyIncomePopulation: 0.71
-    },
-    periodType: 'days',
-    timeToELapse: 58,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-    };
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
 const covid19ImpactEstimator = (data) => {
   const input = data;
   const impact = {};
@@ -20,12 +20,18 @@ const covid19ImpactEstimator = (data) => {
   output.impact = impact;
   output.severeImpact = severeImpact;
   // Challenge1
+  let numDays = data.timeToElapse;
+  if (data.periodType === 'weeks') {
+    numDays = numDays * 7
+  } else if (data.periodType === 'months') {
+    numDays = numDays * 30
+  }
   impact.currentlyInfected = data.reportedCases * 10;
   severeImpact.currentlyInfected = data.reportedCases * 50;
   impact.infectionsByRequestedTime = impact.currentlyInfected
-    * (2 ** Math.floor(data.timeToElaspe / 3));
+    * (2 ** Math.floor(numDays / 3));
   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected
-    * (2 ** Math.floor(data.timeToElaspe / 3));
+    * (2 ** Math.floor(numDays / 3));
   return output;
 };
 export default covid19ImpactEstimator;
